af259816d483d957cd289658dbd0420ba10f642a
Jalil Tahirov <jalil.tahirov@outlook.com>
2019-10-13 13:01:11 +0400

    hw-4 dev environment is installed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # hw4
-Массивы, Ассоциативные массивы
+### Массивы, Ассоциативные массивы
 
 [tasks](https://docs.google.com/document/d/1OHMeFUeEUZUzKHK-6Igb87b2e0FfUFxRZIVJKmBkMBs/edit)
---
+++ tests/index.test.js
@@ -0,0 +1,10 @@
+// describe('', () => {
+//     it('should ...', () => {
+//         //given        
+//         const value = some value'';       
+//         //when
+//         const actual = methodUnderTest(value)
+//         //then
+//         assert.equal(actual, expected)
+//     });
+// })
