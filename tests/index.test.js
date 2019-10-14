4a274ca43b632aec6f396ea8ee0799ef0d0309e4
Jalil Tahirov <jalil.tahirov@outlook.com>
2019-10-14 22:20:05 +0400

    hw-4 removed given when then comments

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,114 +1,103 @@
-// describe('', () => {
-//     it('should ...', () => {
-//         //given        
-//         const value = "some value";       
-//         //when
-//         const actual = methodUnderTest(value)
-//         //then
-//         assert.equal(actual, expected)
-//     });
-// })
 
-// Arraylist tests
 describe('init(array)', () => {
     it('length of the array initially passed should be equal to size of ArrayList ([3,4,5])', () => {
-            //given        
+            
             const array = [3,4,5];
             const sut = new ArrayList();   
             expected =  array.length;  
-            //when            
+                   
             sut.init(array);
             const actual = sut.getSize();
-            //then
+          
             assert.equal(actual, expected)
     });
 });
 
 describe('getSize()', () => {
     it('should return length of current array [1,2,3,4,5] in arraylist ()', () => {
-            //given        
+            
             const array = [1,2,3,4,5];
             const sut = new ArrayList();   
             expected =  array.length;  
-            //when            
+                   
             sut.init(array);
             const actual = sut.getSize();
-            //then
+          
             assert.equal(actual, expected)
     });
 
     it('should return length of current array [] in arraylist ()', () => {
-        //given        
+        
         const array = [];
         const sut = new ArrayList();   
         expected =  array.length;  
-        //when            
+               
         sut.init(array);
         const actual = sut.getSize();
-        //then
+      
         assert.equal(actual, expected)
     });
 
     it('should return 0 if no array initialized in arraylist ()', () => {
-        //given        
+        
         const sut = new ArrayList();   
         expected =  0;  
-        //when
+       
         const actual = sut.getSize();
-        //then
+      
         assert.equal(actual, expected)
     });
 });
 
 describe('clear()', () => {
     it('remove all elements in ArrayList ()', () => {
-        //given        
+        
         const array = [3,4,5];
         const sut = new ArrayList();  
         sut.init(array); 
         expected =  0;  
-        //when
+
         sut.clear();
         const actual = sut.getSize();
-        //then
+
         assert.equal(actual, expected)
     });
 });
 
 describe('toString()', () => {
     it('should return string representation of ArrayList like "[3,4,-5]" ()', () => {
-        //given        
+        
         const array = [3,4,-5];
         const sut = new ArrayList();  
         sut.init(array); 
         expected =  "[3,4,-5]";  
-        //when
+       
         const actual = sut.toString();
-        //then
+      
         assert.equal(actual, expected)
     });
 
     it('should return empty array string from ArrayList like "[]" ()', () => {
-        //given 
+         
         const sut = new ArrayList();  
         expected =  "[]";  
-        //when
+       
         const actual = sut.toString();
-        //then
+      
         assert.equal(actual, expected)
     });
 });
 
 describe('toArray()', () => {
     it('should return array of ArrayList elements ()', () => {
-        //given        
+        
         const array = [3,4,-5];
         const sut = new ArrayList();  
         sut.init(array); 
         expected =  [3,4,-5];  
-        //when
+       
         const actual = sut.toArray();
-        //then
+      
         assert.deepEqual(actual, expected);
     });    
 });
@@ -116,44 +105,44 @@ describe('toArray()', () => {
 // push(number) returns new size of ArrayList.
 describe('push(number)', () => {
     it('should return new size of ArrayList after adding (77)', () => {
-        //given        
+        
         const array = [3,4,-5,66];
         const sut = new ArrayList();  
         sut.init(array); 
         expected =  5;  
-        //when
+       
         const actual = sut.push(77);
-        //then
+      
         assert.equal(actual, expected);
     }); 
 
     it('should return 1 after adding to empty ArrayList (99)', () => {
-        //given
+        
         const sut = new ArrayList();         
         expected =  1;  
-        //when
+       
         const actual = sut.push(99);
-        //then
+      
         assert.equal(actual, expected);
     });  
 
     it('should return current size if NaN to ArrayList (NaN)', () => {
-        //given
+        
         const sut = new ArrayList();         
         expected =  0;  
-        //when
+       
         const actual = sut.push(NaN);
-        //then
+      
         assert.equal(actual, expected);
     }); 
 
     it('should return current size if null to ArrayList (null)', () => {
-        //given
+        
         const sut = new ArrayList();         
         expected =  0;  
-        //when
+       
         const actual = sut.push(null);
-        //then
+      
         assert.equal(actual, expected);
     });               
 });
@@ -161,24 +150,24 @@ describe('push(number)', () => {
 // pop() returns the last element of ArrayList;
 describe('pop()', () => {
     it('should return last element of the arrayList is any exists  ()', () => {
-        //given        
+        
         const array = [3,4,-5,66];
         const sut = new ArrayList();  
         sut.init(array); 
         expected = 66;  
-        //when
+       
         const actual = sut.pop();
-        //then
+      
         assert.equal(actual, expected);
     });    
 
     it('should return undefined if no elements ()', () => {
-        //given
+        
         const sut = new ArrayList();         
         expected =  undefined;  
-        //when
+       
         const actual = sut.pop();
-        //then
+      
         assert.equal(actual, expected);
     });                      
 });
@@ -186,36 +175,36 @@ describe('pop()', () => {
 // unshift(number) returns new size of ArrayList. Принимает в качестве аргумента число или undefined;
 describe('unshift()', () => {
     it('should return new size of arrayList after adding (44)', () => {
-        //given        
+        
         const array = [3,4,-5,66];
         const sut = new ArrayList(); 
         const number = 44;
         sut.init(array); 
         expected = 5;  
-        //when
+       
         const actual = sut.unshift(number);
-        //then
+      
         assert.equal(actual, expected);
     });    
 
     it('should return 1 after adding to empty ArrayList (99)', () => {
-        //given
+        
         const sut = new ArrayList(); 
         const number = 99;        
         const expected =  1;  
-        //when
+       
         const actual = sut.unshift(number);
-        //then
+      
         assert.equal(actual, expected);
     });  
 
     it('should return current size if no arguments passed ()', () => {
-        //given
+        
         const sut = new ArrayList();         
         const expected =  0;  
-        //when
+       
         const actual = sut.unshift();
-        //then
+      
         assert.equal(actual, expected);
     });                           
 });
@@ -223,24 +212,24 @@ describe('unshift()', () => {
 // shift() returns the first element of ArrayLIst;
 describe('shift()', () => {
     it('should return first element of the arrayList if any exists  ()', () => {
-        //given        
+        
         const array = [3,4,-5,66];
         const sut = new ArrayList();  
         sut.init(array); 
         const expected = 3;  
-        //when
+       
         const actual = sut.shift();
-        //then
+      
         assert.equal(actual, expected);
     });    
 
     it('should return undefined if no elements ()', () => {
-        //given
+        
         const sut = new ArrayList();         
         const expected =  undefined;  
-        //when
+       
         const actual = sut.shift();
-        //then
+      
         assert.equal(actual, expected);
     });                      
 });
@@ -248,55 +237,55 @@ describe('shift()', () => {
 //slice(start, end) returns new array, containing elements from start (including) to end (excluding);
 describe('slice(start, end)', () => {
     it('should return [-5,66,6] from [3,4,-5,66,6,8,10]  (2, 5)', () => {
-        //given        
+        
         const array = [3, 4, -5, 66, 6, 8, 10];
         const start = 2;
         const end = 5;
         const sut = new ArrayList();  
         sut.init(array); 
         const expected = [-5, 66, 6];  
-        //when
+       
         const actual = sut.slice(start, end);
-        //then
+      
         assert.deepEqual(actual, expected);
     });    
 
     it('should return current arrayList if no elements ()', () => {
-        //given
+        
         const array = [1,2,3,4,5];
         const sut = new ArrayList();  
         sut.init(array);       
         const expected =  array;  
-        //when
+       
         const actual = sut.slice();
-        //then
+      
         assert.deepEqual(actual, expected);
     });
 
     it('should return only arrayList elements starting from 2nd till end (2)', () => {
-        //given
+        
         const array = [1,2,3,4,5,6];
         const sut = new ArrayList(); 
         const start = 2; 
         sut.init(array);       
         expected =  [3,4,5,6];  
-        //when
+       
         const actual = sut.slice(start);
-        //then
+      
         assert.deepEqual(actual, expected);
     });
 
     it('should return arrayList elements starting from 2nd till end (2,100)', () => {
-        //given
+        
         const array = [1,2,3,4,5,6];
         const start = 2;
         const end = 100;
         const sut = new ArrayList();  
         sut.init(array);       
         expected =  [3,4,5,6];  
-        //when
+       
         const actual = sut.slice(start, end);
-        //then
+      
         assert.deepEqual(actual, expected);
     });
 });
@@ -304,7 +293,7 @@ describe('slice(start, end)', () => {
 //splice(start, numberToSplice, ...newElements) returns array of deleted elements or empty array.
 describe('splice(start, deleteCount, ...newElements)', () => {
     it('should return and delete [-5,66,6] from [3,4,-5,66,6,8,10]  (2, 3)', () => {
-        //given        
+        
         const array = [3, 4, -5, 66, 6, 8, 10];
         const start = 2;
         const deleteCount = 3;
@@ -312,63 +301,63 @@ describe('splice(start, deleteCount, ...newElements)', () => {
         sut.init(array); 
         const expected = [-5, 66, 6];
         const expectedSize = 4;  
-        //when
+       
         const actual = sut.splice(start, deleteCount);
         const actualSize = sut.getSize();
-        //then
+      
         assert.deepEqual(actual, expected);
         assert.equal(actualSize, expectedSize);
     }); 
 
     it('should return and delete [-5,66,6б,8,10] from [3,4,-5,66,6,8,10]  (2)', () => {
-        //given        
+        
         const array = [3, 4, -5, 66, 6, 8, 10];
         const start = 2;
         const sut = new ArrayList();  
         sut.init(array); 
         const expected = [-5, 66, 6, 8, 10];
         const expectedSize = 2;  
-        //when
+       
         const actual = sut.splice(start);
         const actualSize = sut.getSize();
-        //then
+      
         assert.deepEqual(actual, expected);
         assert.equal(actualSize, expectedSize);
     }); 
 
     it('should return and delete [] from [3,4,-5,66,6,8,10]  (2)', () => {
-        //given        
+        
         const array = [3, 4, -5, 66, 6, 8, 10];
         const start = 2;
         const sut = new ArrayList();  
         sut.init(array); 
         const expected = [-5, 66, 6, 8, 10];
         const expectedSize = 2;  
-        //when
+       
         const actual = sut.splice(start);
         const actualSize = sut.getSize();
-        //then
+      
         assert.deepEqual(actual, expected);
         assert.equal(actualSize, expectedSize);
     }); 
 
     it('should return  [] and delete nothing from [3,4,-5,66,6,8,10]  ()', () => {
-        //given        
+        
         const array = [3, 4, -5, 66, 6, 8, 10];      
         const sut = new ArrayList();  
         sut.init(array); 
         const expected = [];
         const expectedSize = 7;  
-        //when
+       
         const actual = sut.splice();
         const actualSize = sut.getSize();
-        //then
+      
         assert.deepEqual(actual, expected);
         assert.equal(actualSize, expectedSize);
     }); 
 
     it('should return and delete [-5,66,6] from [3,4,-5,66,6,8,10] and also add three more elements in parameters  (2, 3, 101, 102, 103)', () => {
-        //given        
+        
         const array = [3, 4, -5, 66, 6, 8, 10];
         const start = 2;
         const deleteCount = 3;
@@ -381,11 +370,11 @@ describe('splice(start, deleteCount, ...newElements)', () => {
         const expectedResult = [-5, 66, 6];
         const expectedSize = 7;
         const expectedToString =  "[3,4,101,102,103,8,10]";
-        //when
+       
         const actualResult = sut.splice(start, deleteCount,elementOne,elementTwo,elementThree);
         const actualSize = sut.getSize();
         const actualToString = sut.toString();
-        //then
+      
         assert.deepEqual(actualResult, expectedResult);
         assert.equal(actualSize, expectedSize);
         assert.equal(actualToString, expectedToString);
